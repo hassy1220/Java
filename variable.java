df98245f00704c03959af963fd1655145172fa91
hassy1220 <hashihassy.1220@gmail.com>
2022-07-04 11:33:39 +0000

    変数

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+  public static void main(String[] args){
+    //データ型(文字列)　変数　= 代入するもので変数に入れれる。
+    String greeting = "Hello paiza";
+    System.out.println(greeting);
+
+    //データ型（数値）　変数　＝　代入するもの
+    int number = 100;
+    System.out.println(number);
+  }
+}
