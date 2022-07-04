000f12e11d19a6f6c4df111f60488b80ab7b081b
hassy1220 <hashihassy.1220@gmail.com>
2022-07-04 11:20:37 +0000

    数値の取り扱い

---
+++ numeric.java
@@ -0,0 +1,7 @@
+public class numeric{
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+        // ダブルクオテーションの有無で数値か文字列かを判断する
+        System.out.println(100+30);
+    }
+}
