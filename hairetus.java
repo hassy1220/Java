7471b01aa80c9abe339da30af9ff9b0e2d937bb4
hassy1220 <hashihassy.1220@gmail.com>
2022-07-08 14:18:25 +0000

    javaプラクティス

---
+++ hairetus.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class hairetus{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    ArrayList<String>array = new ArrayList<String>();
+     while(scan.hasNextLine()){
+       String date = scan.nextLine();
+       array.add(date);
+     }
+     
+     //for(String name : array){
+       System.out.println(array);
+     //}
+    
+  }
+}
